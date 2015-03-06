f1e4beec211590845f49a1ac6fed1661d4241c85
Jean-François Pépin <jfpepin@evollia.com>
2015-03-06 12:58:42 -0500

    Integration de category

--- code/wp-content/themes/quebecnumerique/category.php
+++ code/wp-content/themes/quebecnumerique/category.php
@@ -2,33 +2,39 @@
 
 	<div id="content" role="main">
 
-		<header class="page-header">
+		<header class="page-header header-post-type">
 			<h1 class="page-title"><?php single_cat_title(); ?></h1>
 		</header>
 
-		
+		<p>dsgdfgdfsdfh</p>
 		<div class="c12">
-			<?php if ( have_posts() ) : ?>
+			
+			<div class="nouvelles group cinq img-box">
+				<?php if ( have_posts() ) : ?>
 
-				<?php
-					$category_description = category_description();
-					if ( ! empty( $category_description ) )
-						echo '<div class="archive-meta">' . $category_description . '</div>';
-				?>
+					<?php
+						$category_description = category_description();
+						if ( ! empty( $category_description ) )
+							echo '<div class="archive-meta">' . $category_description . '</div>';
+					?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+					<div>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<article class="c6">
+							<?php include 'content-imgbox.php'; ?>
+						</article>
+					<?php endwhile; ?>
+					</div>
 
-					<?php get_template_part( 'content', get_post_format() ); ?>
+					<?php paging_nav(); ?>
 
-				<?php endwhile; ?>
+				<?php else : ?>
 
-				<?php paging_nav(); ?>
+					<?php get_template_part( 'content', 'none' ); ?>
 
-			<?php else : ?>
+				<?php endif; ?>
+			</div>
 
-				<?php get_template_part( 'content', 'none' ); ?>
-
-			<?php endif; ?>
 		</div>
 
 	</div><!-- #content -->
