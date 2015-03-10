501245a3ae7382f71a871dc6d37790eb4ef7bfed
Jean-François Pépin <jfpepin@evollia.com>
2015-03-10 13:34:36 -0400

    page de résultats de recherche

--- code/wp-content/themes/quebecnumerique/search.php
+++ code/wp-content/themes/quebecnumerique/search.php
@@ -1,46 +1,51 @@
 <?php get_header(); ?>
 
-	<div id="content" class="c12" role="main">
+	<div id="content" role="main">
 
-		<!--header class="page-header">
+		<header class="page-header c12">
 			<h1 class="page-title">
 				<?php printf( __( 'Résultat de recherche pour: %s', THEME_NAME ), '<span></span>' ); ?>
 			</h1>
-		</header-->
+		</header>
 
-		<?php if ( have_posts() ) : ?>
+		<div class="resultats c12">
+			<?php if ( have_posts() ) : ?>
+				<ul>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php
+					$class="" ;
+					$type = get_post_type(); 
+					if ( is_category( '2' ) ||  has_category( '2') ) {$class = 'nouvelles';}
+					else if ( is_category( '3' ) ||  has_category( '3') ) {$class = 'articles';}
+					else if ( $type == 'organisations' ) {$class = 'organisations';}
+					else if ( $type == 'projets' ) {$class = 'projets';}
+					else if ( $type == 'evenements' ) {$class = 'evenements';}
+					else {$class = 'autre';}
+					?>
+					<li class="<?php echo $class ?>">
 
-			<ul class="resultats">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php
-				$class="" ;
-				$type = get_post_type(); 
-				if ( is_category( '2' ) ||  has_category( '2') ) {$class = 'nouvelles';}
-				else if ( is_category( '3' ) ||  has_category( '3') ) {$class = 'articles';}
-				else if ( $type == 'organisations' ) {$class = 'organisations';}
-				else if ( $type == 'projets' ) {$class = 'projets';}
-				else if ( $type == 'evenements' ) {$class = 'evenements';}
-				else {$class = 'autre';}
-				?>
-				<li class="<?php echo $class ?>">
+						<div class="content-wrapper">
+							<div class="content">
+								<h2><?php the_title(); ?></h2>
+							</div>
+						</div>
 
-					<div class="pastille">
-					</div>
+						<div class="pastille">
+						</div>
 
-					<h1><?php echo get_post_type(); ?></h1>
-					<h2><?php the_title(); ?></h2>
-					<h3><?php the_category(); ?></h3>
-					<hr>
-				</li>
-			<?php endwhile; ?>
-			</ul>
-			<?php paging_nav(); ?>
+					</li>
+					<hr class="clear">
 
-		<?php else : ?>
+				<?php endwhile; ?>
+				</ul>
+				<?php paging_nav(); ?>
 
-			<?php get_template_part( 'content', 'none' ); ?>
-				
-		<?php endif; ?>
+			<?php else : ?>
+
+				<?php get_template_part( 'content', 'none' ); ?>
+					
+			<?php endif; ?>
+		</div>
 
 	<hr class="clear" >
 	</div><!-- #content -->
